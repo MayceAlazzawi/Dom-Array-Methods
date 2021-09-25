61203d71d6253651d7da366d6a93d44bf49e05b2
mayce104 <maynab4@gmail.com>
2021-09-25 13:15:17 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillioniresBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
+
+//Add new object to data arr
+function addData(obj) {
+  data.Push(obj);
+
+  updateDOM();
+}
+
+
+ 
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${
+      item.money}`;
+    main.appendChild(element);
+  });
+}
