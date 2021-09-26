09e39bd8f659e662138e7b49b021fab6a758648b
mayce104 <maynab4@gmail.com>
2021-09-26 16:53:26 +0300

    third commit

--- main.js
+++ main.js
@@ -26,15 +26,47 @@ async function getRandomUser() {
   addData(newUser);
 }
 
-//Add new object to data arr
-function addData(obj) {
-  data.Push(obj);
+//Calculate total money
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+// Double eveyones money
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
 
   updateDOM();
 }
 
+//filter only millioniares 
+function showMillionires() {
+  data = data.filter(user => user.money > 1000000);
 
- 
+ updateDOM(); 
+}
+
+//sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+//Add new object to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
 // Update DOM
 function updateDOM(providedData = data) {
   // Clear main div
@@ -43,8 +75,24 @@ function updateDOM(providedData = data) {
   providedData.forEach(item => {
     const element = document.createElement('div');
     element.classList.add('person');
-    element.innerHTML = `<strong>${item.name}</strong> ${
-      item.money}`;
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
     main.appendChild(element);
   });
 }
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+
+
+//event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney)
+sortBtn.addEventListener('click', sortByRichest)
+showMillioniresBtn.addEventListener('click', showMillionires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
